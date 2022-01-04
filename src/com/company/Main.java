faffea0c03f1a17d3a0a7858a5303e2e744519e0
mattvoci <mvoci@taboracademy.org>
2022-01-03 20:04:53 -0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        On January 3, 2022 I added the GitHub CoPilot Plug-in
+        This currently in a developers-only phase
+
+        I am on a waitlist to be approved.
+         */
+
+        System.out.println("Hello GitHub");
+    }
+}
